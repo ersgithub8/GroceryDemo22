ad899cd032b9b05cc2dac259ec839839a5003fc6
hamzali4642 <55617940+hamzali4642@users.noreply.github.com>
2020-07-01 10:34:10 +0500

    changes

--- app/src/main/java/Fragment/Category_Fragment.java
+++ app/src/main/java/Fragment/Category_Fragment.java
@@ -59,6 +59,7 @@ public class Category_Fragment extends Fragment {
     private TextView textView;
     Inerface inerface;
 
+    View viewb=null;
     Product_adapter product_adapter;
     List<Product_model> product_models=new ArrayList<>();
     RecyclerView recyclerView;
@@ -102,7 +103,7 @@ public class Category_Fragment extends Fragment {
 //        rv_headre_icons.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
 
         make_menu_items();
-
+        product_adapter = new Product_adapter(product_models,getActivity());
         rv_headre_icons.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), rv_headre_icons, new RecyclerTouchListener.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
@@ -112,7 +113,20 @@ public class Category_Fragment extends Fragment {
 
                 getProducts(menu_models.get(position).getId());
 
-//                view.setBackgroundColor(Color.parseColor("#ff0000"));
+                if(view != viewb){
+                if(viewb ==  null){
+
+                    view.setBackgroundColor(getResources().getColor(R.color.green));
+                    viewb=view;
+                }else {
+                    view.setBackgroundColor(getResources().getColor(R.color.green));
+                    viewb.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    viewb=view;
+                }
+                }
+
+
+
 //                view.setPressed(true);/
 
             }
@@ -208,6 +222,7 @@ public class Category_Fragment extends Fragment {
                             Type listType = new TypeToken<List<Product_model>>() {
                             }.getType();
                             product_models = gson.fromJson(response.getString("data"), listType);
+
                             product_adapter = new Product_adapter(product_models,getActivity());
                             recyclerView.setVisibility(View.VISIBLE);
                             textView.setVisibility(View.GONE);
