341e4ea7138d97c99ef3817878746259e7aba32f
Bilal912 <bilalt586@gmail.com>
2021-06-20 23:30:42 -0700

    Bilal

--- app/src/main/java/gogrocer/tcc/WebView.java
+++ app/src/main/java/gogrocer/tcc/WebView.java
@@ -6,6 +6,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -14,13 +15,15 @@ import android.widget.ProgressBar;
 
 import java.nio.file.WatchEvent;
 
+import Fragment.Terms_and_Condition_fragment;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class WebView extends AppCompatActivity {
 android.webkit.WebView webView;
-ProgressBar mProgress;
 SharedPreferences sharedPreferences;
 String language="";
+ProgressBar progressBar;
+String url = "https://tawk.to/chat/605c2160f7ce18270933af93/1f1ju4pbd";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,30 +34,39 @@ String language="";
         }else {
             setTheme(R.style.AppTheme);
         }
+
         setContentView(R.layout.activity_web_view);
 
         webView = findViewById(R.id.webView_id);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("https://tawk.to/chat/605c2160f7ce18270933af93/1f1ju4pbd");
+        progressBar = (ProgressBar) findViewById(R.id.progress);
 
-        webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(android.webkit.WebView view, WebResourceRequest request) {
+        webView.setWebChromeClient( new WebView.MyWebChromeClient());
+        webView.setWebViewClient( new WebView.webClient());
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
 
-                return super.shouldOverrideUrlLoading(view, request);
-            }
-        });
+    }
 
-        webView.setWebChromeClient(new WebChromeClient()
-        {
-            @Override
-            public void onProgressChanged(android.webkit.WebView view, int newProgress) {
-                super.onProgressChanged(view, newProgress);
+    public class MyWebChromeClient extends WebChromeClient {
+        public void onProgressChanged(android.webkit.WebView view, int newProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(newProgress);
+        }
+    }
 
-            }
-        });
+    public class webClient extends WebViewClient {
+        public boolean  shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
 
+        @Override
+        public void onPageFinished(android.webkit.WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
 }
